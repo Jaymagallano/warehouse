6b6e1cbbe35221c6a584ff696097ab7b10de8dee
Jaymagallano <jaymagallano2004@gmail.com>
2025-12-16 01:10:13 +0800

    Update routing configuration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,12 +19,48 @@ $routes->group('auth', function($routes) {
 // Warehouse Manager Routes
 $routes->group('warehouse-manager', ['filter' => 'auth'], function($routes) {
     $routes->get('dashboard', 'WarehouseManagerController::dashboard');
+    
+    // Inventory CRUD
     $routes->get('inventory-overview', 'WarehouseManagerController::inventoryOverview');
+    $routes->get('inventory/list', 'WarehouseManagerController::getInventoryList');
+    $routes->post('inventory/add', 'WarehouseManagerController::addInventory');
+    $routes->get('inventory/(:num)', 'WarehouseManagerController::getInventory/$1');
+    $routes->post('inventory/update/(:num)', 'WarehouseManagerController::updateInventory/$1');
+    $routes->post('inventory/delete/(:num)', 'WarehouseManagerController::deleteInventory/$1');
+    
+    // Receiving CRUD
     $routes->get('receiving', 'WarehouseManagerController::receiving');
+    $routes->get('receiving/list', 'WarehouseManagerController::getReceivingList');
+    $routes->post('receiving/add', 'WarehouseManagerController::addReceiving');
+    $routes->get('receiving/(:num)', 'WarehouseManagerController::getReceiving/$1');
+    $routes->post('receiving/update/(:num)', 'WarehouseManagerController::updateReceiving/$1');
+    $routes->post('receiving/delete/(:num)', 'WarehouseManagerController::deleteReceiving/$1');
+    
+    // Shipping CRUD
     $routes->get('shipping', 'WarehouseManagerController::shipping');
+    $routes->get('shipping/list', 'WarehouseManagerController::getShippingList');
+    $routes->post('shipping/add', 'WarehouseManagerController::addShipping');
+    $routes->get('shipping/(:num)', 'WarehouseManagerController::getShipping/$1');
+    $routes->post('shipping/update/(:num)', 'WarehouseManagerController::updateShipping/$1');
+    $routes->post('shipping/delete/(:num)', 'WarehouseManagerController::deleteShipping/$1');
+    
+    // Approvals CRUD
     $routes->get('approvals', 'WarehouseManagerController::approvals');
+    $routes->get('approvals/list', 'WarehouseManagerController::getApprovalsList');
+    $routes->post('approvals/approve/(:num)', 'WarehouseManagerController::approveRequest/$1');
+    $routes->post('approvals/reject/(:num)', 'WarehouseManagerController::rejectRequest/$1');
+    
+    // Batch Tracking CRUD
     $routes->get('batch-tracking', 'WarehouseManagerController::batchTracking');
+    $routes->get('batch/list', 'WarehouseManagerController::getBatchList');
+    $routes->post('batch/add', 'WarehouseManagerController::addBatch');
+    $routes->get('batch/(:num)', 'WarehouseManagerController::getBatch/$1');
+    $routes->post('batch/update/(:num)', 'WarehouseManagerController::updateBatch/$1');
+    $routes->post('batch/delete/(:num)', 'WarehouseManagerController::deleteBatch/$1');
+    
+    // Reports
     $routes->get('reports', 'WarehouseManagerController::reports');
+    $routes->get('reports/generate', 'WarehouseManagerController::generateReport');
 });
 
 // Inventory Auditor Routes
